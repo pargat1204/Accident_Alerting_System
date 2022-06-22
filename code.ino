bb881d0b3d4000ce902d2e1168c03e52ea21a75e
PARGAT SINGH <107990987+pargat1204@users.noreply.github.com>
2022-06-22 23:26:22 +0530

    Add files via upload

---
+++ code.ino
@@ -0,0 +1,21 @@
+int buzzer=11;
+int sensor=8;
+int state = 0;
+
+void setup() {
+pinMode(11,OUTPUT);
+pinMode(8,INPUT);// put your setup code here, to run once:
+
+}
+
+void loop() {
+  state=digitalRead(8);
+  if (state == HIGH)
+  {
+    digitalWrite(11,HIGH);// put your main code here, to run repeatedly:
+  }
+else
+{
+  digitalWrite(11, LOW);
+}
+}
